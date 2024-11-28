924bd047466c7233eb48ab197f5556307ded3caa
Bart Versluijs <bartversluijs@icloud.com>
2024-11-28 16:15:24 +0100

    feat(properties): updated `PropertyModel` to newest version
    Issue: IN-2227

--- types.go
+++ types.go
@@ -103,36 +103,64 @@ type AddressModel struct {
 }
 
 type PropertyModel struct {
-	// The property id ,
+	// The property id
 	ID string `json:"id"`
+
 	// The code for the property that can be shown in reports and table views
 	Code string `json:"code"`
+
+	// The id of the property used as a template while creating the property
+	PropertyTemplateID string `json:"propertyTemplateId"`
+
+	// Whether the property can be used as a template for other properties
+	IsTemplate bool `json:"isTemplate"`
+
 	// The name for the property
 	Name map[string]string `json:"name"`
+
 	// The description for the property
 	Description map[string]string `json:"description"`
+
 	// The legal name of the company running the property.
 	CompanyName string `json:"companyName"`
+
 	// The managing director(s) of the company, as they should appear on invoices
 	ManagingDirectors string `json:"managingDirectors"`
-	// The entry in the Commercial Reigster of the company running the property, as it should appear on invoices ,
+
+	// The entry in the Commercial Register of the company running the property, as it should appear on invoices
 	CommercialRegisterEntry string `json:"commercialRegisterEntry"`
+
 	// The Tax-ID of the company running the property, as it should appear on invoices
 	TaxID string `json:"taxId"`
-	// The location of the property ,
-	Location    AddressModel     `json:"location"`
-	BankAccount BankAccountModel `json:"bankAccountModel"`
-	// The payment terms used for all rate plans
+
+	// The location of the property
+	Location AddressModel `json:"location"`
+
+	BankAccount BankAccountModel `json:"bankAccount"`
+
+	// 	The payment terms used for all rate plans
 	PaymentTerms map[string]string `json:"paymentTerms"`
-	// The time zone name of the property from the IANA Time Zone Database. (see:
-	// https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
+
+	// The time zone name of the property from the IANA Time Zone Database.
+	// (see: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
 	TimeZone string `json:"timeZone"`
+
 	// The currency a property works with.
 	CurrencyCode string `json:"currencyCode"`
+
 	// Date of creation
-	// Specify a date and time (without fractional second part) in UTC or with UTC
-	// offset as defined in the ISO8601:2004
+	// Specify a date and time (without fractional second part) in UTC or with
+	// UTC offset as defined in the ISO8601:2004
 	Created Date `json:"created"`
+
+	// The status of the property
+	Status string `json:"status"`
+
+	// Is the property archived
+	IsArchived bool `json:"isArchived"`
+
+	// The list of actions for this property
+	Actions []ActionModel `json:"actions"`
 }
 
 type BankAccountModel struct {
