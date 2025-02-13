7fd91a48bee38f1ca62df4a748364737343d06a1
Bart Versluijs <bartversluijs@icloud.com>
2025-02-13 14:41:20 +0100

    feat: added support for `PostGroupBooking`

---
+++ post_group_booking_test.go
@@ -0,0 +1,51 @@
+package apaleo_test
+
+import (
+	"encoding/json"
+	"log"
+	"testing"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/omniboost/go-apaleo"
+)
+
+func TestPostGroupBooking(t *testing.T) {
+	client := client()
+	req := client.NewPostGroupBookingRequest()
+	req.RequestBody().Name = "API test"
+	req.RequestBody().PaymentAccount = apaleo.CreatePaymentAccountModel{
+		AccountNumber:  "123456789",
+		AccountHolder:  "John Doe",
+		ExpiryMonth:    "1",
+		ExpiryYear:     "2025",
+		PaymentMethod:  "CreditCard",
+		PayerEmail:     "a@b.c",
+		PayerReference: "12345678910",
+		IsVirtual:      false,
+	}
+
+	req.RequestBody().Booker = apaleo.BookerModel{
+		Title:     "Mr",
+		Gender:    "Male",
+		FirstName: "John",
+		LastName:  "Doe",
+		Email:     "a@b.c",
+		Phone:     "123456789",
+		Address: apaleo.PersonAddressModel{
+			AddressLine1: "123 Main St",
+			AddressLine2: "Apt 1",
+			PostalCode:   "12345",
+			City:         "New York",
+			CountryCode:  "US",
+		},
+	}
+
+	req.RequestBody().PropertyIDs = []string{"UTR"}
+	resp, err := req.Do()
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := json.MarshalIndent(resp, "", "  ")
+	log.Println(string(b))
+}
