4316b255d1120f09db5cabbba94485918e2ec8bb
Bart Versluijs <bartversluijs@icloud.com>
2025-03-15 11:34:51 +0100

    fix: use `CommaSeparatedQueryParam` for `ExternalReferences` while doing a reservation lookup

--- get_reservations_nsfw.go
+++ get_reservations_nsfw.go
@@ -32,38 +32,39 @@ func (c *Client) NewGetReservationsNSFWQueryParams() *GetReservationsNSFWQueryPa
 }
 
 type GetReservationsNSFWQueryParams struct {
-	BookingID                 string   `schema:"booking_id,omitempty"`
-	PropertyIDs               []string `schema:"propertyIds,omitempty"`
-	RatePlanIDs               []string `schema:"ratePlanIds,omitempty"`
-	CompanyIDs                []string `schema:"companyIds,omitempty"`
-	UnitIDs                   []string `schema:"unitIds,omitempty"`
-	UnitGroupIDs              []string `schema:"unitGroupIds,omitempty"`
-	UnitGroupTypes            []string `schema:"unitGroupTypes,omitempty"`
-	BlockIDs                  []string `schema:"blockIds,omitempty"`
-	MarketSegmentIDs          []string `schema:"marketSegmentIds,omitempty"`
-	Status                    []string `schema:"status,omitempty"`
-	DateFilter                string   `schema:"dateFilter,omitempty"`
-	From                      DateTime `schema:"from,omitempty"`
-	To                        DateTime `schema:"to,omitempty"`
-	ChannelCode               []string `schema:"channelCode,omitempty"`
-	Sources                   []string `schema:"sources,omitempty"`
-	ValidationMessageCategory []string `schema:"validationMessageCategory,omitempty"`
-	ExternalCode              string   `schema:"externalCode,omitempty"`
-	TextSearch                string   `schema:"textSearch,omitempty"`
-	ExternalReferences        []string `schema:"externalReferences,omitempty"`
-	BalanceFilter             []string `schema:"balanceFilter,omitempty"`
-	AllFoliosHaveInvoice      bool     `schema:"allFoliosHaveInvoice,omitempty"`
-	IsPreCheckedIn            bool     `schema:"isPreCheckedIn,omitempty"`
-	PageNumber                int      `schema:"pageNumber,omitempty"`
-	PageSize                  int      `schema:"pageSize,omitempty"`
-	Sort                      []string `schema:"sort,omitempty"`
-	Expand                    []string `schema:"expand,omitempty"`
+	BookingID                 string                   `schema:"booking_id,omitempty"`
+	PropertyIDs               []string                 `schema:"propertyIds,omitempty"`
+	RatePlanIDs               []string                 `schema:"ratePlanIds,omitempty"`
+	CompanyIDs                []string                 `schema:"companyIds,omitempty"`
+	UnitIDs                   []string                 `schema:"unitIds,omitempty"`
+	UnitGroupIDs              []string                 `schema:"unitGroupIds,omitempty"`
+	UnitGroupTypes            []string                 `schema:"unitGroupTypes,omitempty"`
+	BlockIDs                  []string                 `schema:"blockIds,omitempty"`
+	MarketSegmentIDs          []string                 `schema:"marketSegmentIds,omitempty"`
+	Status                    []string                 `schema:"status,omitempty"`
+	DateFilter                string                   `schema:"dateFilter,omitempty"`
+	From                      DateTime                 `schema:"from,omitempty"`
+	To                        DateTime                 `schema:"to,omitempty"`
+	ChannelCode               []string                 `schema:"channelCode,omitempty"`
+	Sources                   []string                 `schema:"sources,omitempty"`
+	ValidationMessageCategory []string                 `schema:"validationMessageCategory,omitempty"`
+	ExternalCode              string                   `schema:"externalCode,omitempty"`
+	TextSearch                string                   `schema:"textSearch,omitempty"`
+	ExternalReferences        CommaSeparatedQueryParam `schema:"externalReferences,omitempty"`
+	BalanceFilter             []string                 `schema:"balanceFilter,omitempty"`
+	AllFoliosHaveInvoice      bool                     `schema:"allFoliosHaveInvoice,omitempty"`
+	IsPreCheckedIn            bool                     `schema:"isPreCheckedIn,omitempty"`
+	PageNumber                int                      `schema:"pageNumber,omitempty"`
+	PageSize                  int                      `schema:"pageSize,omitempty"`
+	Sort                      []string                 `schema:"sort,omitempty"`
+	Expand                    []string                 `schema:"expand,omitempty"`
 }
 
 func (p GetReservationsNSFWQueryParams) ToURLValues() (url.Values, error) {
 	encoder := utils.NewSchemaEncoder()
 	encoder.RegisterEncoder(Date{}, utils.EncodeSchemaMarshaler)
 	encoder.RegisterEncoder(DateTime{}, utils.EncodeSchemaMarshaler)
+	encoder.RegisterEncoder(CommaSeparatedQueryParam{}, utils.EncodeSchemaMarshaler)
 	params := url.Values{}
 
 	err := encoder.Encode(p, params)
