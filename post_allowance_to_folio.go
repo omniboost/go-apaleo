87acbb222cd7afad9cb0be4a5658f2cb06d44fcc
Bart Versluijs <bartversluijs@icloud.com>
2025-03-20 18:51:59 +0100

    feat: added support for `PostAllowanceToFolio`

---
+++ post_allowance_to_folio.go
@@ -0,0 +1,143 @@
+package apaleo
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/omniboost/go-apaleo/utils"
+)
+
+func (c *Client) NewPostAllowanceToFolioRequest() PostAllowanceToFolioRequest {
+	return PostAllowanceToFolioRequest{
+		client:      c,
+		queryParams: c.NewPostAllowanceToFolioQueryParams(),
+		pathParams:  c.NewPostAllowanceToFolioPathParams(),
+		method:      http.MethodPost,
+		headers:     http.Header{},
+		requestBody: c.NewPostAllowanceToFolioRequestBody(),
+	}
+}
+
+type PostAllowanceToFolioRequest struct {
+	client      *Client
+	queryParams *PostAllowanceToFolioQueryParams
+	pathParams  *PostAllowanceToFolioPathParams
+	method      string
+	headers     http.Header
+	requestBody PostAllowanceToFolioRequestBody
+}
+
+func (c *Client) NewPostAllowanceToFolioQueryParams() *PostAllowanceToFolioQueryParams {
+	return &PostAllowanceToFolioQueryParams{}
+}
+
+type PostAllowanceToFolioQueryParams struct{}
+
+func (p PostAllowanceToFolioQueryParams) ToURLValues() (url.Values, error) {
+	encoder := utils.NewSchemaEncoder()
+	encoder.RegisterEncoder(Date{}, utils.EncodeSchemaMarshaler)
+	encoder.RegisterEncoder(DateTime{}, utils.EncodeSchemaMarshaler)
+	encoder.RegisterEncoder(CommaSeparatedQueryParam{}, utils.EncodeSchemaMarshaler)
+	params := url.Values{}
+
+	err := encoder.Encode(p, params)
+	if err != nil {
+		return params, err
+	}
+
+	return params, nil
+}
+
+func (r *PostAllowanceToFolioRequest) QueryParams() *PostAllowanceToFolioQueryParams {
+	return r.queryParams
+}
+
+func (c *Client) NewPostAllowanceToFolioPathParams() *PostAllowanceToFolioPathParams {
+	return &PostAllowanceToFolioPathParams{}
+}
+
+type PostAllowanceToFolioPathParams struct {
+	ID string `schema:"id"`
+}
+
+func (p *PostAllowanceToFolioPathParams) Params() map[string]string {
+	return map[string]string{
+		"id": p.ID,
+	}
+}
+
+func (r *PostAllowanceToFolioRequest) PathParams() *PostAllowanceToFolioPathParams {
+	return r.pathParams
+}
+
+func (r *PostAllowanceToFolioRequest) PathParamsInterface() PathParams {
+	return r.pathParams
+}
+
+func (r *PostAllowanceToFolioRequest) SetMethod(method string) {
+	r.method = method
+}
+
+func (r *PostAllowanceToFolioRequest) Method() string {
+	return r.method
+}
+
+func (s *Client) NewPostAllowanceToFolioRequestBody() PostAllowanceToFolioRequestBody {
+	return PostAllowanceToFolioRequestBody{}
+}
+
+type PostAllowanceToFolioRequestBody struct {
+	ServiceType  string             `json:"serviceType"`
+	VatType      string             `json:"vatType"`
+	SubAccountID string             `json:"subAccountId,omitempty"`
+	Reason       string             `json:"reason,omitempty"`
+	Amount       MonetaryValueModel `json:"amount"`
+	BusinessDate *Date              `json:"businessDate,omitempty"`
+}
+
+func (r PostAllowanceToFolioRequest) NewRequestBody() PostAllowanceToFolioRequestBody {
+	return PostAllowanceToFolioRequestBody{}
+}
+
+func (r *PostAllowanceToFolioRequest) RequestBody() *PostAllowanceToFolioRequestBody {
+	return &r.requestBody
+}
+
+func (r *PostAllowanceToFolioRequest) RequestBodyInterface() interface{} {
+	return r.requestBody
+}
+
+func (r *PostAllowanceToFolioRequest) SetRequestBody(body PostAllowanceToFolioRequestBody) {
+	r.requestBody = body
+}
+
+func (r *PostAllowanceToFolioRequest) NewResponseBody() *PostAllowanceToFolioResponseBody {
+	return &PostAllowanceToFolioResponseBody{}
+}
+
+type PostAllowanceToFolioResponseBody struct {
+	ID string `json:"id"`
+}
+
+func (r *PostAllowanceToFolioRequest) URL() *url.URL {
+	u := r.client.GetEndpointURL("finance/v1/folio-actions/{{.id}}/allowances", r.PathParams())
+	return &u
+}
+
+func (r *PostAllowanceToFolioRequest) Do() (PostAllowanceToFolioResponseBody, error) {
+	// Create http request
+	req, err := r.client.NewRequest(nil, r)
+	if err != nil {
+		return *r.NewResponseBody(), err
+	}
+
+	// Process query parameters
+	err = utils.AddQueryParamsToRequest(r.QueryParams(), req, true)
+	if err != nil {
+		return *r.NewResponseBody(), err
+	}
+
+	responseBody := r.NewResponseBody()
+	_, err = r.client.Do(req, responseBody)
+	return *responseBody, err
+}
---
+++ post_allowance_to_folio_test.go
@@ -0,0 +1,36 @@
+package apaleo_test
+
+import (
+	"encoding/json"
+	"log"
+	"testing"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/omniboost/go-apaleo"
+)
+
+func TestPostAllowanceToFolio(t *testing.T) {
+	client := client()
+	req := client.NewPostAllowanceToFolioRequest()
+	req.PathParams().ID = "IMJWEMLG-AMZ2-1"
+
+	body := req.NewRequestBody()
+	body.ServiceType = "Other"
+	body.VatType = "Null"
+	body.SubAccountID = "AMZ2-OTHERMIGRATION"
+	body.Reason = "Refund: AMZ-F65001-2333866"
+	body.Amount = apaleo.MonetaryValueModel{
+		Currency: "EUR",
+		Amount:   3.5,
+	}
+
+	req.SetRequestBody(body)
+
+	resp, err := req.Do()
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := json.MarshalIndent(resp, "", "  ")
+	log.Println(string(b))
+}
